0080913b9f76e681773887bf5078e0d9272ca8b2
KaurGR <kaur-gabriel.rajasalu@tktk.ee>
2024-04-05 22:31:47 +0300

    Add Week7-Functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter the operation (+/-):"); 
+char userOperator = Char.Parse(Console.ReadLine());
+Console.WriteLine("Enter first number:");
+int num1 = Int32.Parse(Console.ReadLine()); 
+Console.WriteLine("Enter second number:");
+int num2 = Int32.Parse(Console.ReadLine());
+
+switch (userOperator)
+{
+    case '+':
+        Addition(num1, num2);
+        break;
+    case '-':
+        Subtraction(num1, num2);
+        break;
+    default:
+        Console.WriteLine("invalid operator");
+        break;
+}    
+
+static void Addition(int a, int b)
+{
+    Console.WriteLine($"{a} + {b} = {a + b}");
+}
+static void Subtraction(int a, int b)
+{
+    Console.WriteLine($"{a} - {b} = {a - b}");
+}
